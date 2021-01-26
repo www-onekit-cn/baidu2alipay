1817ec8fc5bf289d3fa9bbadb7e2c2e240c9358f
lyxlwz <54365306+lyxlwz@users.noreply.github.com>
2021-01-26 18:07:19 +0800

    .

--- miniprogram_dev/li/li.js
+++ miniprogram_dev/li/li.js
@@ -490,90 +490,90 @@ var app = getApp()
 //   })
 
 // image
-// OnekitPage({
-//     data:{
-//         cutArray:[
-//           {
-//               mode:'top',
-//               text:'top：不缩放图片，只显示图片的顶部区域'
-//             },
-//           {
-//               mode:'bottom',
-//               text:'bottom：不缩放图片，只显示图片的底部区域'
-//             },
-//           {
-//               mode:'center',
-//               text:'center：不缩放图片，只显示图片的中间区域'
-//             },
-//           {
-//               mode:'left',
-//               text:'left：不缩放图片，只显示图片的左边区域'
-//             },
-//           {
-//               mode:'right',
-//               text:'right：不缩放图片，只显示图片的右边边区域'
-//             },
-//           {
-//               mode:'top left',
-//               text:'top left：不缩放图片，只显示图片的左上边区域'
-//             },
-//           {
-//               mode:'top right',
-//               text:'top right：不缩放图片，只显示图片的右上边区域'
-//             },
-//           {
-//               mode:'bottom left',
-//               text:'bottom left：不缩放图片，只显示图片的左下边区域'
-//             },
-//           {
-//               mode:'bottom right',
-//               text:'bottom right：不缩放图片，只显示图片的右下边区域'
-//             }
-//         ],
-//         scaleArray:[
-//           {
-//               mode:'scaleToFill',
-//               text:'scaleToFill：不保持纵横比缩放图片，使图片完全适应'
-//             },
-//           {
-//               mode:'aspectFit',
-//               text:'aspectFit：保持纵横比缩放图片，使图片的长边能完全显示出来',
-//               hasBackgroud:1
-//             },
-//           {
-//               mode:'aspectFill',
-//               text:'aspectFill：保持纵横比缩放图片，只保证图片的短边能完全显示出来'
-//             },
-//           {
-//               mode:'widthFix',
-//               text:'widthFix：宽度不变，高度自动变化，保持原图宽高比不变'
-//             }
-//         ],
-//         src:'https://b.bdstatic.com/miniapp/images/demo-dog.png'
-//       },
-//     onShow:function(){
-//       const openParams = app.globalData.openParams
-//       if(openParams){
-//       swan.reportAnalytics('pageshow',{
-//           fr:openParams,
-//           type:'component',
-//           name:'image'
-//         });
-//     }
-//     },
-//     onHide:function(){
-//       app.globalData.openParams = ''
-//     },
-//     imageError:function(e){
-//       console.log('image 发生 error 事件，携带值为',e.detail.errMsg)
-//     },
-//     onTap:function(e){
-//       console.log('image 发生 tap 事件',e)
-//     },
-//     imageLoad:function(e){
-//       console.log('image 加载成功',e.type)
-//     }
-//   })
+OnekitPage({
+    data:{
+        cutArray:[
+          {
+              mode:'top',
+              text:'top：不缩放图片，只显示图片的顶部区域'
+            },
+          {
+              mode:'bottom',
+              text:'bottom：不缩放图片，只显示图片的底部区域'
+            },
+          {
+              mode:'center',
+              text:'center：不缩放图片，只显示图片的中间区域'
+            },
+          {
+              mode:'left',
+              text:'left：不缩放图片，只显示图片的左边区域'
+            },
+          {
+              mode:'right',
+              text:'right：不缩放图片，只显示图片的右边边区域'
+            },
+          {
+              mode:'top left',
+              text:'top left：不缩放图片，只显示图片的左上边区域'
+            },
+          {
+              mode:'top right',
+              text:'top right：不缩放图片，只显示图片的右上边区域'
+            },
+          {
+              mode:'bottom left',
+              text:'bottom left：不缩放图片，只显示图片的左下边区域'
+            },
+          {
+              mode:'bottom right',
+              text:'bottom right：不缩放图片，只显示图片的右下边区域'
+            }
+        ],
+        scaleArray:[
+          {
+              mode:'scaleToFill',
+              text:'scaleToFill：不保持纵横比缩放图片，使图片完全适应'
+            },
+          {
+              mode:'aspectFit',
+              text:'aspectFit：保持纵横比缩放图片，使图片的长边能完全显示出来',
+              hasBackgroud:1
+            },
+          {
+              mode:'aspectFill',
+              text:'aspectFill：保持纵横比缩放图片，只保证图片的短边能完全显示出来'
+            },
+          {
+              mode:'widthFix',
+              text:'widthFix：宽度不变，高度自动变化，保持原图宽高比不变'
+            }
+        ],
+        src:'https://b.bdstatic.com/miniapp/images/demo-dog.png'
+      },
+    onShow:function(){
+      const openParams = app.globalData.openParams
+      if(openParams){
+      swan.reportAnalytics('pageshow',{
+          fr:openParams,
+          type:'component',
+          name:'image'
+        });
+    }
+    },
+    onHide:function(){
+      app.globalData.openParams = ''
+    },
+    imageError:function(e){
+      console.log('image 发生 error 事件，携带值为',e.detail.errMsg)
+    },
+    onTap:function(e){
+      console.log('image 发生 tap 事件',e)
+    },
+    imageLoad:function(e){
+      console.log('image 加载成功',e.type)
+    }
+  })
 
 //video
 // OnekitPage({
@@ -1026,46 +1026,46 @@ var app = getApp()
 
 
 //movable-view
-OnekitPage({
-    data:{
-        title:'movable-view',
-        x:30,
-        y:30,
-        scale:1
-      },
-    onShow:function(){
-      const openParams = app.globalData.openParams
-      if(openParams){
-      swan.reportAnalytics('pageshow',{
-          fr:openParams,
-          type:'component',
-          name:'movable-view'
-        });
-    }
-    },
-    onHide:function(){
-      app.globalData.openParams = ''
-    },
-    move:function(){
-      this.setData({
-        x1:50,
-        y1:50
-      })
-    },
-    scale:function(){
-      this.setData({
-        scale:3
-      })
-    },
-    onChange:function(e){
-      console.log(e.detail)
-    },
-    onScale:function(e){
-      console.log(e.detail)
-    },
-    movableView:function(){
-      swan.navigateTo({
-        url:'/component/nav-movable-view/nav-movable-view'
-      })
-    }
-  })
+// OnekitPage({
+//     data:{
+//         title:'movable-view',
+//         x:30,
+//         y:30,
+//         scale:1
+//       },
+//     onShow:function(){
+//       const openParams = app.globalData.openParams
+//       if(openParams){
+//       swan.reportAnalytics('pageshow',{
+//           fr:openParams,
+//           type:'component',
+//           name:'movable-view'
+//         });
+//     }
+//     },
+//     onHide:function(){
+//       app.globalData.openParams = ''
+//     },
+//     move:function(){
+//       this.setData({
+//         x1:50,
+//         y1:50
+//       })
+//     },
+//     scale:function(){
+//       this.setData({
+//         scale:3
+//       })
+//     },
+//     onChange:function(e){
+//       console.log(e.detail)
+//     },
+//     onScale:function(e){
+//       console.log(e.detail)
+//     },
+//     movableView:function(){
+//       swan.navigateTo({
+//         url:'/component/nav-movable-view/nav-movable-view'
+//       })
+//     }
+//   })
